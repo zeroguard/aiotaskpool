670501cf271a384b01b1f0db1d6357b6bc2bd7c4
vsmysle <kyrylo.voronchenko@gmail.com>
2020-04-30 18:07:34 +0300

    added basic test, added dependencies to Pipfile

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,7 +1,23 @@
 """Test all placeholder."""
 import pytest
+import random
+import asyncio
+
+from aiotaskpool import TaskPool
 
 
-def test_placeholder():
+async def wait_and_echo(item):
+    rand = random.random()
+    await asyncio.sleep(rand)
+    return rand
+
+
+@pytest.mark.asyncio
+async def test_placeholder():
     """."""
-    pass
+    items = range(10)
+    task_pool = TaskPool(32)
+    async with task_pool.map(wait_and_echo, items) as results:
+        results.stats.enable()
+        async for result in results:
+            assert result
